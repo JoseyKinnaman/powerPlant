b5b4fc59d567b659c4397fb9478494fbc561a23d
Jozy Kinnaman <jozypants@gmail.com>
2020-06-23 15:33:07 -0700

    add functionality for a second plant.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,41 @@ import $ from "jquery";
 import { storeState, changeState } from './../src/plant.js';
 
 $(document).ready(function() {
+  // let counter = 0;
   const stateChanger = storeState();
+  const stateChanger2 = storeState();
+ 
   const blueFood = changeState("soil")(5);
   const blueLight = changeState("light")(20);
-  $('#feed').click(function(){
+  const blueWater = changeState("water")(15);
+
+   $('#feed').click(function(){
     const newState = stateChanger(blueFood);
-    
     $('#soil-value').text(newState.soil);
   });
 
   $('#light').click(function() {
     const newState = stateChanger(blueLight);
     $('#light-value').text(newState.light);
+  });
+
+  $('#water').click(function () {
+    const newState = stateChanger(blueWater);
+    $('#water-value').text(newState.water);
+  });
+
+  $('#feed2').click(function(){
+    const newState = stateChanger2(blueFood);
+    $('#soil2-value').text(newState.soil);
+  });
+
+  $('#light2').click(function() {
+    const newState = stateChanger2(blueLight);
+    $('#light2-value').text(newState.light);
+  });
+
+  $('#water2').click(function () {
+    const newState = stateChanger2(blueWater);
+    $('#water2-value').text(newState.water);
   });
 });
--- src/plant.js
+++ src/plant.js
@@ -1,7 +1,7 @@
 // This function stores our state.
 export const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction) => {
+  let currentState = {}; // {}
+  return (stateChangeFunction) => { 
     const newState = stateChangeFunction(currentState);
     currentState = {...newState} ;
     return newState;
@@ -18,11 +18,82 @@ export const changeState = (prop) => {
   }
 }
 
+
+// const plantState = () => {
+//   let currentPlants = [];
+//   return(plantObj) => {
+//     currentPlants.push(plantObj);
+//   };
+// };
+
+// const plant = { soil: 0, light: 0, water: 0, plantId: 0 };
+
+
+ // funtion that takes in a function updates its own state objecttion that takes in a function updates its own state objecttion that takes in a function updates its own state objecttion that updates its own state object    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Functions for our factory 
 
 //export const blueFood = changeState("soil")(5)
 // const greenFood = changeState("soil")(10)
 // const yuckyFood = changeState("soil")(-5)
+
+
 
 
 
